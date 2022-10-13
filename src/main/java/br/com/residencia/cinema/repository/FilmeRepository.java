1864d58ff15150665743a508dabf913519bfb7c9
Nanami78 <110745971+Nanami78@users.noreply.github.com>
2022-10-13 18:02:53 -0300

    trabalho atualizado

--- src/main/java/br/com/residencia/cinema/repository/FilmeRepository.java
+++ src/main/java/br/com/residencia/cinema/repository/FilmeRepository.java
@@ -2,6 +2,7 @@ package br.com.residencia.cinema.repository;
 
 import java.util.UUID;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.residencia.cinema.entity.Filme;
